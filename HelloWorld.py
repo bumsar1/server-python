15e007b53ddcd90ad03fb7afa15a51691732e6d9
bumsar1 <gustav.lydeking@gmail.com>
2026-06-03 10:37:26 +0200

    test

--- HelloWorld.py
+++
@@ -1,5 +0,0 @@
-list = ["Mouse", "Keyoard", "Computer"]
-list.append("Monitor")
-list[0] = "Laptop"
-list.sort()
-print(list)
--- Loop.py
+++
@@ -1,4 +0,0 @@
-myMessage = "Hello, World!"
-
-for i in range(1, 30001, 10000):
-    print(myMessage + " " + str(i))
